e2ec05c1230cf0ec1da83264102ee49e3cd918ad
Manuk77 <manmovsesyan1985@gmail.com>
2023-06-20 13:45:45 +0400

    done capacityConstraint() method

--- bank/src/main/java/com/example/bank/bank_model/portfolio/Portfolio.java
+++ bank/src/main/java/com/example/bank/bank_model/portfolio/Portfolio.java
@@ -1,6 +1,6 @@
 package com.example.bank.bank_model.portfolio;
-import com.example.bank.customer.creating_requests.requests.CustomerRequest;
 
+import com.example.bank.customer.creating_requests.requests.CustomerRequest;
 
 
 import java.util.*;
@@ -10,7 +10,10 @@ public class Portfolio {
     private List<CustomerRequest> customerRequests;
     private List<CustomerWithMathModelFields> customersMath;
 
+    private Map<Float, List<Integer>> mapL = new HashMap<>();
+    private List<Float> L = new ArrayList<>();
     private List<Integer> acceptableLoan;
+
     public Portfolio() {
     }
 
@@ -41,57 +44,53 @@ public class Portfolio {
 
     /**
      * L = Σ(σi^2 * ni^2 * wi^2) -> min
-     *  Σ(ri * ni * wi) >= R
-     *  Σ(wi * ni) <= 1
-     *  ni ∈ {0, 1}, i = 1, ..., N
-     * @return List of integers
+     * Σ(ri * ni * wi) >= R
+     * Σ(wi * ni) <= 1
+     * ni ∈ {0, 1}, i = 1, ..., N
      * possible values of list {0, 1}
      */
-    private void capacityConstraint() {
+    private void capacityConstraint(final List<Integer> listN) {
 
-        List<List<Integer>> list = allPossibleOptions();
+
         float sum = 0.0f;
-        final long[] r = {0L};
+        long[] r = {0L};
         customersMath.forEach(x -> r[0] += x.getR());
         long R = (long) (r[0] * 1.1);
-        List<Float> L = new ArrayList<>();
-        Map<Float, List<Integer>> mapL = new HashMap<>();
-        for (List<Integer> integers : list) {
+        for (int i = 0; i < customersMath.size(); ++i) {
+            sum += customersMath.get(i).getW() * listN.get(i);
+        }
+
+        if (sum <= 1) {
+            long sum1 = 0L;
             for (int i = 0; i < customersMath.size(); ++i) {
-                sum += customersMath.get(i).getW() * integers.get(i);
+                sum1 += (long) (customersMath.get(i).getR() * listN.get(i) * customersMath.get(i).getW());
             }
-
-            if (sum <= 1) {
-                long sum1 = 0L;
+            if (sum1 >= R) {
+                float summ = 0.0f;
                 for (int i = 0; i < customersMath.size(); ++i) {
-                    sum1 += (long) (customersMath.get(i).getR() * integers.get(i) * customersMath.get(i).getW());
-                }
-                if (sum1 >= R) {
-                    float summ = 0.0f;
-                    for (int i = 0; i < customersMath.size(); ++i) {
-                       summ += (float) (Math.pow(customersMath.get(i).getSigma(), 2) *
-                                Math.pow(integers.get(i), 2) *
-                                Math.pow(customersMath.get(i).getW(), 2));
+                    summ += (float) (Math.pow(customersMath.get(i).getSigma(), 2) *
+                            Math.pow(listN.get(i), 2) *
+                            Math.pow(customersMath.get(i).getW(), 2));
 
-                    }
-                    L.add(summ);
-                    mapL.put(summ, integers);
                 }
+                L.add(summ);
+                mapL.put(summ, listN);
             }
         }
 
-        acceptableLoan = mapL.get(Collections.min(L));
+
 
     }
 
     /**
      * optimal loans customer lists
+     *
      * @return acceptable customerRequest loans list
      */
     private List<CustomerRequest> optimalLoans() {
         List<CustomerRequest> optimalLoans = new ArrayList<>();
-        for (int i = 0; i < acceptableLoan.size(); ++i ) {
-            if ((acceptableLoan.get(i) & 1) == 1)
+        for (int i = 0; i < acceptableLoan.size(); ++i) {
+            if (acceptableLoan.get(i) == 1)
                 optimalLoans.add(customerRequests.get(i));
         }
         return optimalLoans;
@@ -99,8 +98,12 @@ public class Portfolio {
 
 
     /**
-     * this method calculates all possible options of binary vector
-     * @return List<List<Integers>> where are all possible options
+     * this method calculates all possible options of binary list:
+     * it calculates one possible option and passes to capacityConstraint() witch
+     * checks whether conforms to the constraints, if it does it calculates L = Σ(σi^2 * ni^2 * wi^2)
+     * and adds in the list L:
+     * when all possible options are calculated, finds in list L min element,
+     * and gets from with that key the binary list of N:
      * ex. [1, 0, 0, 0, ..., 0],
      *     [0, 1, 0, 0, ..., 0]
      *      .  .  .  .  ...  .
@@ -108,7 +111,7 @@ public class Portfolio {
      *      .  .  .  .  ...  .
      *     [1, 1, 1, 1, ..., 1]
      */
-    private List<List<Integer>> allPossibleOptions() {
+    private void allPossibleOptions() {
         int k = customersMath.size(); // Length of the list
         int totalOptions = (int) Math.pow(2, k) - 1;
 
@@ -122,10 +125,10 @@ public class Portfolio {
                 option.add(bit);
             }
 
-            optionsList.add(option);
+            capacityConstraint(option);
         }
 
-        return optionsList;
+        acceptableLoan = mapL.get(Collections.min(L));
     }
 
 
